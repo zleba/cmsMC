0f422ac70ab7f7094493b7be1545815b18c6c50f
Radek Zlebcik <zlebcr@mail.desy.de>
2018-11-05 00:46:36 +0100

    to merge everyting

--- merge.sh
+++ merge.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #setup version for merge of yoda files
 cmsRel=CMSSW_10_1_0
 source /cvmfs/cms.cern.ch/cmsset_default.sh
@@ -13,9 +14,20 @@ eval `scram runtime -sh`
 scram b
 cd ../../
 
-for dd in farm/[A-Z]*
+#merge individual MCs
+for dd in farm/[A-Z]*/[A-Z]*
 do
    d=`basename $dd`
-   yodamerge -o farm/$d/${d}.yoda farm/$d/histos/Rivet*.yoda
-
+   yodamerge -o $dd/${d}.yoda $dd/histos/Rivet*.yoda
 done
+
+#merge slices
+for dd in farm/[A-Z]*/
+do
+   d=`basename $dd`
+
+   yodamerge --add -o $dd/${d}.yoda   $dd/*/*.yoda
+done
+
+#plot rivet
+rivet-mkhtml --mc-errs -o farm/qcdPlots   farm/*/*.yoda
